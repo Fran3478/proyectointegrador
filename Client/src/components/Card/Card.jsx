1d4fdeba3eaff9bae1bae07e6ed6c9a287f4ecfd
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-07-21 12:53:48 -0300

    quick fix

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import {addFav, removeFav } from '../../redux/actions/actions'
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-//import { connect } from 'react-redux';
-
 
 export default function Card(props) {
    const dispatch = useDispatch()
@@ -57,75 +55,3 @@ export default function Card(props) {
       </div>
    );
 }
-
-
-
-
-// function Card(props) {
-//    const [isFav, setIsFav] = useState(false)
-//    function handleFavorite(){
-//       if(isFav){
-//          setIsFav(false)
-//          props.removeFav(props.id)
-//       } else {
-//          setIsFav(true)
-//          props.addFav(props)
-//       }
-//    }
-//    useEffect(() => {
-//       props.myFavorites.forEach((fav) => {
-//          if (fav.id === props.id) {
-//             setIsFav(true);
-//          }
-//       });
-//    }, [props.myFavorites]);
-//    return (
-//       <div className={style.div}>
-//          {props.onClose ? <button className={style.close} onClick={() => {props.onClose(props.id)}}>X</button> : null}
-         
-//          <Link to={`/detail/${props.id}`}>
-//             <div className={style.divImg}>
-//                <img className={style.img} src={props.image} alt={props.name} />
-//                <div className={style.divP}>
-//                   <p className={style.name}>{props.name}</p>
-//                </div>
-//             </div>
-//          </Link>
-         
-//          <div className={style.conteiner}>
-//             <div className={style.divText}>
-//                <h2 className={style.status}> Status: {props.status}</h2>
-//                <h2 className={style.species}>Species: {props.species}</h2>
-//                <h2 className={style.gender}>Gender: {props.gender}</h2>
-//             </div>
-            
-//             {
-//                isFav ? (
-//                   <button className={style.fav} onClick={handleFavorite}>❤️</button>
-//                ) : (
-//                   <button className={style.fav} onClick={handleFavorite}>🤍</button>
-//                )
-//             }
-//          </div>
-//       </div>
-//    );
-// }
-
-// export function mapDispatchToProps(dispatch){
-//    return {
-//       addFav: function(character){
-//          dispatch(addFav(character))
-//       },
-//       removeFav: function(id){
-//          dispatch(removeFav(id))
-//       }
-//    }
-// }
-
-// export function mapStateToProps(globalState){
-//    return {
-//       myFavorites: globalState.myFavorites
-//    }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Card)
